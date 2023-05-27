6e4dcd2aa3145b55ae8396f72ea386d91052d9ed
Yasin <111612847+YasinenfaL@users.noreply.github.com>
2023-05-27 17:06:26 +0300

    hotel_pipeline.py

--- hotel_pipeline.py
+++ hotel_pipeline.py
@@ -13,45 +13,6 @@ pd.set_option("display.max_columns", None)
 
 
 def grab_col_names(dataframe, cat_th=10, car_th=20):
-    """
-
-    Veri setindeki kategorik, numerik ve kategorik fakat kardinal değişkenlerin isimlerini verir.
-    Not: Kategorik değişkenlerin içerisine numerik görünümlü kategorik değişkenler de dahildir.
-
-    Parameters
-    ------
-        dataframe: dataframe
-                Değişken isimleri alınmak istenilen dataframe
-        cat_th: int, optional
-                numerik fakat kategorik olan değişkenler için sınıf eşik değeri
-        car_th: int, optinal
-                kategorik fakat kardinal değişkenler için sınıf eşik değeri
-
-    Returns
-    ------
-        cat_cols: list
-                Kategorik değişken listesi
-        num_cols: list
-                Numerik değişken listesi
-        cat_but_car: list
-                Kategorik görünümlü kardinal değişken listesi
-
-    Examples
-    ------
-        import seaborn as sns
-        df = sns.load_dataset("iris")
-        print(grab_col_names(df))
-
-
-    Notes
-    ------
-        cat_cols + num_cols + cat_but_car = toplam değişken sayısı
-        num_but_cat cat_cols'un içerisinde.
-        Return olan 3 liste toplamı toplam değişken sayısına eşittir: cat_cols + num_cols + cat_but_car = değişken sayısı
-
-    """
-
-    # cat_cols, cat_but_car
     cat_cols = [col for col in dataframe.columns if dataframe[col].dtypes == "O"]
     num_but_cat = [col for col in dataframe.columns if dataframe[col].nunique() < cat_th and
                    dataframe[col].dtypes != "O"]
@@ -60,16 +21,15 @@ def grab_col_names(dataframe, cat_th=10, car_th=20):
     cat_cols = cat_cols + num_but_cat
     cat_cols = [col for col in cat_cols if col not in cat_but_car]
 
-    # num_cols
     num_cols = [col for col in dataframe.columns if dataframe[col].dtypes != "O"]
     num_cols = [col for col in num_cols if col not in num_but_cat]
 
-    # print(f"Observations: {dataframe.shape[0]}")
-    # print(f"Variables: {dataframe.shape[1]}")
-    # print(f'cat_cols: {len(cat_cols)}')
-    # print(f'num_cols: {len(num_cols)}')
-    # print(f'cat_but_car: {len(cat_but_car)}')
-    # print(f'num_but_cat: {len(num_but_cat)}')
+     print(f"Observations: {dataframe.shape[0]}")
+     print(f"Variables: {dataframe.shape[1]}")
+     print(f'cat_cols: {len(cat_cols)}')
+     print(f'num_cols: {len(num_cols)}')
+     print(f'cat_but_car: {len(cat_but_car)}')
+     print(f'num_but_cat: {len(num_but_cat)}')
     return cat_cols, num_cols, cat_but_car
 
 
@@ -201,5 +161,5 @@ def main():
 
 
 if __name__ == "__main__":
-    print("İşlem başladı")
+    print("Process Begins")
     main()
